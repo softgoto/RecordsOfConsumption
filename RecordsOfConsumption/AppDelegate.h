6940cfe82afc6c4fb67b03f95ff890d54dba2cb3
softgoto <softgoto@gmail.com>
2015-06-04 14:46:49 +0800

    Initial Commit

---
+++ RecordsOfConsumption/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecordsOfConsumption
+//
+//  Created by xuhui on 15/6/4.
+//  Copyright (c) 2015年 xuhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecordsOfConsumption/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecordsOfConsumption
+//
+//  Created by xuhui on 15/6/4.
+//  Copyright (c) 2015年 xuhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
